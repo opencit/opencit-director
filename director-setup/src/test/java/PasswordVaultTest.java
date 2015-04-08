b56c20fe09eb4dfe3661426edd27539781a6d8e2
Ryan Savino <ryan.k.savino@intel.com>
2015-04-07 18:11:03 -0700

    dev-1.x-sprint-7-merge: fixed conditional statement for KMS basic login password retrieval from environment and configuration fixed password string retrieval from PasswordVault in setup task DirectorEnvelopeKeyRegistration Added junit test for retrieving password from PasswordVault

---
+++ director-setup/src/test/java/PasswordVaultTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2015 Intel Corporation
+ * All rights reserved.
+ */
+
+import com.intel.dcsg.cpg.crypto.key.password.Password;
+import static com.intel.mtwilson.configuration.ConfigurationFactory.getConfiguration;
+import com.intel.mtwilson.configuration.PasswordVaultFactory;
+import com.intel.mtwilson.util.crypto.keystore.PasswordKeyStore;
+import java.io.IOException;
+import java.security.KeyStoreException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Arrays;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author rksavino
+ */
+public class PasswordVaultTest {
+    
+    public PasswordVaultTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    
+    @Test
+    public void testPasswordVault() throws KeyStoreException, IOException, InvalidKeySpecException {
+        String KMS_LOGIN_BASIC_PASSWORD = "kms.login.basic.password";
+        String kmsLoginBasicPassword = "password";
+
+        try (PasswordKeyStore passwordVault = PasswordVaultFactory.getPasswordKeyStore(getConfiguration())) {
+            passwordVault.set(KMS_LOGIN_BASIC_PASSWORD, new Password(kmsLoginBasicPassword.toCharArray()));
+        }
+        try (PasswordKeyStore passwordVault = PasswordVaultFactory.getPasswordKeyStore(getConfiguration())) {
+            if (passwordVault.contains(KMS_LOGIN_BASIC_PASSWORD)) {
+                String newKmsLoginBasicPassword = new String(passwordVault.get(KMS_LOGIN_BASIC_PASSWORD).toCharArray());
+                System.out.println("KMS LOGIN BASIC PASSWORD: " + newKmsLoginBasicPassword);
+            }
+        }
+        
+        
+    }
+}
