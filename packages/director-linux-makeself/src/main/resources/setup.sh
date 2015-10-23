bc670e2e446d4fb0b2267e60a318388f7c938357
boski <boski.shah@intel.com>
2015-10-22 17:12:12 -0700

    dev-1.x-beta2-tdws-2269-input-validataion: Updated installer to validate image store and MTW credentials

--- packages/director-linux-makeself/src/main/resources/setup.sh
+++ packages/director-linux-makeself/src/main/resources/setup.sh
@@ -190,6 +190,9 @@ update_property_in_file "director.id" "$DIRECTOR_PROPERTIES_FILE" "$DIRECTOR_ID"
 update_property_in_file "vm.whitelist.hash.type" "$DIRECTOR_PROPERTIES_FILE" "$VM_WHITELIST_HASH_TYPE"
 prompt_with_default IMAGE_STORE_TYPE "Image Store Type:" "$IMAGE_STORE_TYPE"
 update_property_in_file "image.store.type" "$DIRECTOR_PROPERTIES_FILE" "$IMAGE_STORE_TYPE"
+if [ $IMAGE_STORE_TYPE != "Openstack_Glance" ]; then
+	echo_failure "Image store type $IMAGE_STORE_TYPE is not supported. Supported type is: Openstack_Glance"
+fi
 prompt_with_default IMAGE_STORE_SERVER "Image Store Server:" "$IMAGE_STORE_SERVER"
 update_property_in_file "image.store.server" "$DIRECTOR_PROPERTIES_FILE" "$IMAGE_STORE_SERVER"
 prompt_with_default IMAGE_STORE_USERNAME "Image Store Username:" "$IMAGE_STORE_USERNAME"
@@ -198,7 +201,15 @@ prompt_with_default_password IMAGE_STORE_PASSWORD "Image Store Password:" "$IMAG
 update_property_in_file "image.store.password" "$DIRECTOR_PROPERTIES_FILE" "$IMAGE_STORE_PASSWORD"
 prompt_with_default TENANT_NAME "Tenant Name:" "$TENANT_NAME"
 update_property_in_file "tenant.name" "$DIRECTOR_PROPERTIES_FILE" "$TENANT_NAME"
-
+#validatign image store credentials
+if [ $IMAGE_STORE_TYPE == "Openstack_Glance" ]; then
+	http_status_code=`curl -i -d '{"auth": {"tenantName": "'$TENANT_NAME'", "passwordCredentials": {"username": "'$IMAGE_STORE_USERNAME'", "password": "'$IMAGE_STORE_PASSWORD'"}}}'  -H "Content-type: application/json" http://$IMAGE_STORE_SERVER:5000/v2.0/tokens 2>/dev/null | head -n 1 | cut -d$' ' -f2`
+	if [ $http_status_code == "200" ]; then
+			echo "$IMAGE_STORE_TYPE credentials are validated successfully"
+	else
+			echo_failure "Can not connect to $IMAGE_STORE_TYPE using given credentials"
+	fi
+fi
 # modifying after mtwilson api client built
 prompt_with_default MTWILSON_SERVER "Mtwilson Server:" "$MTWILSON_SERVER"
 update_property_in_file "mtwilson.server" "$DIRECTOR_PROPERTIES_FILE" "$MTWILSON_SERVER"
@@ -208,6 +219,14 @@ prompt_with_default MTWILSON_USERNAME "Mtwilson Username:" "$MTWILSON_USERNAME"
 update_property_in_file "mtwilson.username" "$DIRECTOR_PROPERTIES_FILE" "$MTWILSON_USERNAME"
 prompt_with_default_password MTWILSON_PASSWORD "Mtwilson Password:" "$MTWILSON_PASSWORD"
 update_property_in_file "mtwilson.password" "$DIRECTOR_PROPERTIES_FILE" "$MTWILSON_PASSWORD"
+
+#validating MTW credentials 
+http_status_code=`curl --insecure -i -X GET "https://$MTWILSON_SERVER:$MTWILSON_SERVER_PORT/mtwilson/v2/hosts?nameEqualTo=nameEw" -u $MTWILSON_USERNAME:$MTWILSON_PASSWORD 2>/dev/null | head -n 1 | cut -d$' ' -f2`
+if [ $http_status_code == "200" ] || [ $http_status_code == "500" ]; then
+        echo "MtWilson credentials are validated successfully"
+else
+        echo_failure "Can not connect to MtWilson using given credentials"
+fi
 
 # director requires java 1.7 or later
 # detect or install java (jdk-1.7.0_51-linux-x64.tar.gz)
