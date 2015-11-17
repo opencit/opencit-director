f4f8095dba9f1fe73af5f0e775775b84b1ab8c62
Ryan Savino <ryan.k.savino@intel.com>
2015-11-16 18:07:01 -0800

    dev-cit-3.0-sprint3: added call to disable_tcp_timestamps in functions

--- packages/director/src/main/resources/setup.sh
+++ packages/director/src/main/resources/setup.sh
@@ -506,9 +506,7 @@ fi
 # register linux startup script
 register_startup_script $DIRECTOR_HOME/bin/director.sh director
 
-
-
-
+disable_tcp_timestamps
 
 # setup the director, unless the NOSETUP variable is defined
 if [ -z "$DIRECTOR_NOSETUP" ]; then
