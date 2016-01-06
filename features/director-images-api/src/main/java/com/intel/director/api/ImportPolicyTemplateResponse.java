a947d3602a6f52350b13e9698e2618fafac057af
SOakx <siddharthx.oak@intel.com>
2016-01-06 14:28:03 +0530

    Merge branch 'dev-cit-3.0-sprint3' into release-cit-3.0-api-refactor fix merge conflict issue

--- features/director-images-api/src/main/java/com/intel/director/api/ImportPolicyTemplateResponse.java
+++ features/director-images-api/src/main/java/com/intel/director/api/ImportPolicyTemplateResponse.java
@@ -2,7 +2,6 @@ package com.intel.director.api;
 
 public class ImportPolicyTemplateResponse {
 
-<<<<<<< HEAD
 	public String status;
 	public String details;
 	public String trust_policy;
@@ -18,27 +17,22 @@ public class ImportPolicyTemplateResponse {
 	public void setDetails(String details) {
 		this.details = details;
 	}
-=======
 
 	public String trust_policy;
 	public String error;
 	
->>>>>>> dev-cit-3.0-sprint3
 	public String getTrust_policy() {
 		return trust_policy;
 	}
 	public void setTrust_policy(String trust_policy) {
 		this.trust_policy = trust_policy;
 	}
-<<<<<<< HEAD
-=======
 	public String getError() {
 		return error;
 	}
 	public void setError(String error) {
 		this.error = error;
 	}
->>>>>>> dev-cit-3.0-sprint3
 	
 	
 }
