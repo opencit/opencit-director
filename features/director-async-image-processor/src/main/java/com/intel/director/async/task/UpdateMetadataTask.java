f0b0fb61aeee41559637833d7d7105ef385dbaa8
Siddharth Oak <soakx@intel.com>
2016-03-03 11:02:41 +0530

    dev-cit-3.1-swift_integration: Fixes for updating latest uploaed image with policy URI

--- features/director-async-image-processor/src/main/java/com/intel/director/async/task/UpdateMetadataTask.java
+++ features/director-async-image-processor/src/main/java/com/intel/director/async/task/UpdateMetadataTask.java
@@ -16,13 +16,8 @@ import com.intel.director.api.ImageActionTask;
 import com.intel.director.api.ImageStoreTransferObject;
 import com.intel.director.api.ImageStoreUploadTransferObject;
 import com.intel.director.api.PolicyUploadTransferObject;
-import com.intel.director.api.ui.ImageStoreUploadFields;
 import com.intel.director.api.ui.ImageStoreUploadFilter;
-import com.intel.director.api.ui.ImageStoreUploadOrderBy;
-import com.intel.director.api.ui.OrderByEnum;
-import com.intel.director.api.ui.PolicyUploadFields;
 import com.intel.director.api.ui.PolicyUploadFilter;
-import com.intel.director.api.ui.PolicyUploadOrderBy;
 import com.intel.director.common.Constants;
 import com.intel.director.images.exception.DirectorException;
 import com.intel.director.store.StoreManager;
@@ -37,8 +32,7 @@ import com.intel.mtwilson.trustpolicy.xml.TrustPolicy;
  * @author GS-0681
  */
 public class UpdateMetadataTask extends GenericUploadTask {
-	public static final org.slf4j.Logger log = org.slf4j.LoggerFactory
-			.getLogger(UpdateMetadataTask.class);
+	public static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(UpdateMetadataTask.class);
 
 	public UpdateMetadataTask() throws DirectorException {
 		super();
@@ -70,126 +64,128 @@ public class UpdateMetadataTask extends GenericUploadTask {
 	public boolean runUpdateMetadataTask() {
 		boolean runFlag = true;
 
-	
+		try {
+			trustPolicy = persistService.fetchPolicyById(imageInfo.getTrust_policy_id());
+		} catch (DbException e1) {
+			log.error("Unable to fetch trustpolicy for tp id::" + imageInfo.getTrust_policy_id(), e1);
+			updateImageActionState(Constants.ERROR, "Error in Updating metadata in imagestore");
+			return false;
+		}
 
-			try {
-				trustPolicy = persistService.fetchPolicyById(imageInfo
-						.getTrust_policy_id());
-			} catch (DbException e1) {
-				log.error("Unable to fetch trustpolicy for tp id::"+imageInfo
-						.getTrust_policy_id(),e1);
-				updateImageActionState(Constants.ERROR, "Error in Updating metadata in imagestore");
-				return false;
-			}
+		JAXBContext jaxbContext = null;
+		Unmarshaller unmarshaller = null;
+		try {
+			jaxbContext = JAXBContext.newInstance(TrustPolicy.class);
+			unmarshaller = (Unmarshaller) jaxbContext.createUnmarshaller();
+		} catch (JAXBException e1) {
+			log.error("JaxbException upadetmetadata task", e1);
+			updateImageActionState(Constants.ERROR, "Error in Updating metadata in imagestore");
+			return false;
+		}
 
-			JAXBContext jaxbContext =null;
-			Unmarshaller unmarshaller =null;
-			try {
-				jaxbContext = JAXBContext
-						.newInstance(TrustPolicy.class);
-				unmarshaller = (Unmarshaller) jaxbContext
-						.createUnmarshaller();
-			} catch (JAXBException e1) {
-				log.error("JaxbException upadetmetadata task",e1);
-				updateImageActionState(Constants.ERROR, "Error in Updating metadata in imagestore");
-				return false;
-			}
+		StringReader reader = new StringReader(trustPolicy.getTrust_policy());
+		TrustPolicy policy = null;
+		try {
+			policy = (TrustPolicy) unmarshaller.unmarshal(reader);
+		} catch (JAXBException e1) {
+			log.error("JaxbException upadetmetadata task", e1);
+			updateImageActionState(Constants.ERROR, "Error in Updating metadata in imagestore");
+			return false;
+		}
 
-			StringReader reader = new StringReader(
-					trustPolicy.getTrust_policy());
-			TrustPolicy policy =null;
-			try {
-				policy = (TrustPolicy) unmarshaller.unmarshal(reader);
-			} catch (JAXBException e1) {
-				log.error("JaxbException upadetmetadata task",e1);
-				updateImageActionState(Constants.ERROR,"Error in Updating metadata in imagestore");
-				return false;
-			}
-			String glanceId = policy.getImage().getImageId();
+		String glanceId = policy.getImage().getImageId();
 
-			PolicyUploadFilter policyUploadFilter = new PolicyUploadFilter();
-			policyUploadFilter.setTrust_policy_id(trustPolicy.getId());
-			
-						
-			
-			List<PolicyUploadTransferObject> policyUploads =null;
-			try {
-				policyUploads = persistService
-						.fetchPolicyUploads(policyUploadFilter, null);
-			} catch (DbException e1) {
-				log.error("Error fetchPolicyUploads",e1);
-				updateImageActionState(Constants.ERROR, "Error in Updating metadata in imagestore");
-				return false;
+		ImageStoreUploadTransferObject imageStoreTranserObject = null;
+		ImageStoreUploadFilter imgUpFilter = new ImageStoreUploadFilter();
+		imgUpFilter.setImage_id(imageInfo.getId());
+		List<ImageStoreUploadTransferObject> fetchImageUploads = null;
+		try {
+			fetchImageUploads = persistService.fetchImageUploads(imgUpFilter, null);
+			if ((fetchImageUploads != null && fetchImageUploads.size() > 0)) {
+				imageStoreTranserObject = fetchImageUploads.get(fetchImageUploads.size() - 1);
+				log.info("Last uploaded image to store : {},  glance id : {}", imageStoreTranserObject.getStoreId(),
+						imageStoreTranserObject.getStoreArtifactId());
+				log.info("Glance id from policy {}", glanceId);
 			}
-			PolicyUploadTransferObject policyUploadTransferObject = policyUploads.get(policyUploads.size()-1);
-			String policyUri = policyUploadTransferObject.getPolicy_uri();
-			String storeId=policyUploadTransferObject.getStoreId();
-			ImageStoreTransferObject imageStoreDTO=null;
-			try {
+		} catch (DbException e) {
+			log.error("Error fetching image uploads {}", e);
+			runFlag = false;
+			updateImageActionState(Constants.ERROR, "Error in update metadata task");
+
+		}
+
+		if (!imageStoreTranserObject.getStoreArtifactId().equals(glanceId)) {
+			glanceId = imageStoreTranserObject.getStoreArtifactId();
+		}
+
+		PolicyUploadFilter policyUploadFilter = new PolicyUploadFilter();
+		policyUploadFilter.setTrust_policy_id(trustPolicy.getId());
+
+		List<PolicyUploadTransferObject> policyUploads = null;
+		try {
+			policyUploads = persistService.fetchPolicyUploads(policyUploadFilter, null);
+		} catch (DbException e1) {
+			log.error("Error fetchPolicyUploads", e1);
+			updateImageActionState(Constants.ERROR, "Error in Updating metadata in imagestore");
+			return false;
+		}
+		PolicyUploadTransferObject policyUploadTransferObject = policyUploads.get(policyUploads.size() - 1);
+		String policyUri = policyUploadTransferObject.getPolicy_uri();
+		String storeId = policyUploadTransferObject.getStoreId();
+		ImageStoreTransferObject imageStoreDTO = null;
+		try {
 			imageStoreDTO = persistService.fetchImageStorebyId(storeId);
-			} catch (DbException e) {
-				log.error("No store exists for id {}", storeId);
-				
-			}
-			String connectorName = imageStoreDTO.getConnector();
-			String trustPolicyLocationTag=connectorName.toLowerCase()+":"+policyUri;
-			customProperties.put(Constants.MTWILSON_TRUST_POLICY_LOCATION,
-					trustPolicyLocationTag);
-			customProperties.put(Constants.GLANCE_ID, glanceId);
-		////	customProperties.put(Constants.NAME, trustPolicy.getDisplay_name());
-			StoreManager imageStoreManager =null;
-			try {
-				imageStoreManager = StoreManagerFactory
-						.getStoreManager(taskAction.getStoreId());
-			} catch (StoreException e1) {
-				log.error("Error in getting image store manager",e1);
-				updateImageActionState(Constants.ERROR, "Error in Updating metadata in imagestore");
-				return false;
-			}
-			ImageActionTask imageActionTask = getImageActionTaskFromArray();
-	
-			try {
-				imageStoreManager.addCustomProperties(customProperties);
-			} catch (StoreException e1) {
-				log.error("Error in addCustomProperties for image store manager",e1);
-				updateImageActionState(Constants.ERROR, "Error in Updating metadata in imagestore");
-				return false;
-			}
-			
-			if (imageActionTask != null && imageActionTask.getUri() == null) {
+		} catch (DbException e) {
+			log.error("No store exists for id {}", storeId);
 
-				try {
-					imageStoreManager.update();
-				} catch (StoreException e) {
-					runFlag = false;
-					log.error("Error updating imageuploads",e);
-					updateImageActionState(Constants.ERROR, "Error in Updating metadata task");
-					return false;
-				}
+		}
+		String connectorName = imageStoreDTO.getConnector();
+		String trustPolicyLocationTag = connectorName.toLowerCase() + ":" + policyUri;
+		customProperties.put(Constants.MTWILSON_TRUST_POLICY_LOCATION, trustPolicyLocationTag);
+		customProperties.put(Constants.GLANCE_ID, glanceId);
+		//// customProperties.put(Constants.NAME,
+		//// trustPolicy.getDisplay_name());
+		StoreManager imageStoreManager = null;
+		try {
+			imageStoreManager = StoreManagerFactory.getStoreManager(taskAction.getStoreId());
+		} catch (StoreException e1) {
+			log.error("Error in getting image store manager", e1);
+			updateImageActionState(Constants.ERROR, "Error in Updating metadata in imagestore");
+			return false;
+		}
+		ImageActionTask imageActionTask = getImageActionTaskFromArray();
 
-			}
-			
-			ImageStoreUploadFilter imgUpFilter = new ImageStoreUploadFilter();
-			imgUpFilter.setImage_id(imageInfo.getId());
-			List<ImageStoreUploadTransferObject> fetchImageUploads = null;
+		try {
+			imageStoreManager.addCustomProperties(customProperties);
+		} catch (StoreException e1) {
+			log.error("Error in addCustomProperties for image store manager", e1);
+			updateImageActionState(Constants.ERROR, "Error in Updating metadata in imagestore");
+			return false;
+		}
+
+		if (imageActionTask != null && imageActionTask.getUri() == null) {
+
 			try {
-				fetchImageUploads = persistService.fetchImageUploads(
-						imgUpFilter, null);
-				log.info("ImageActionImpl, ARTIFACT_POLICY action, fetchImageUploads");
-				if ((fetchImageUploads != null && fetchImageUploads.size() > 0)) {
-					ImageStoreUploadTransferObject imageStoreTranserObject=fetchImageUploads.get(fetchImageUploads.size() - 1);
-					imageStoreTranserObject.setPolicyUploadId(trustPolicy.getId());
-					persistService.updateImageUpload(imageStoreTranserObject);
-				}
-			} catch (DbException e) {
-				log.error("Error fetching image uploads {}",e);
+				imageStoreManager.update();
+			} catch (StoreException e) {
 				runFlag = false;
-				updateImageActionState(Constants.ERROR,"Error in update metadata task");
-				
+				log.error("Error updating imageuploads", e);
+				updateImageActionState(Constants.ERROR, "Error in Updating metadata task");
+				return false;
 			}
-			
 
-	
+		}
+
+		try {
+			imageStoreTranserObject.setPolicyUploadId(trustPolicy.getId());
+			persistService.updateImageUpload(imageStoreTranserObject);
+		} catch (DbException e) {
+			log.error("Error fetching image uploads {}", e);
+			runFlag = false;
+			updateImageActionState(Constants.ERROR, "Error in update metadata task");
+
+		}
+
 		if (runFlag) {
 			updateImageActionState(Constants.COMPLETE, "Update Metadata task completed");
 		}
