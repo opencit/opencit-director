bf6ab843582d62a18778c28f1a17b2cf9e18f43f
Siddharth Oak <soakx@intel.com>
2016-04-01 15:24:01 +0530

    dev-cit-3.1-swift_integration: fix file upload issue

--- director-common/src/main/java/com/intel/director/common/ImageDeploymentHashTypeCache.java
+++ director-common/src/main/java/com/intel/director/common/ImageDeploymentHashTypeCache.java
@@ -3,19 +3,23 @@ package com.intel.director.common;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.intel.mtwilson.trustpolicy.xml.DigestAlgorithm;
 
 public class ImageDeploymentHashTypeCache extends DirectorPropertiesCache {
+	private static final Logger log = LoggerFactory.getLogger(ImageDeploymentHashTypeCache.class);
+
 	private static Map<String, String> deploymentTypeHashTypeMap = new HashMap<String, String>();
 
 	public static void init() {
 		getAllValues();
-		deploymentTypeHashTypeMap.put(Constants.DEPLOYMENT_TYPE_VM,
-				properties.getProperty(Constants.HASH_TYPE_VM));
+		deploymentTypeHashTypeMap.put(Constants.DEPLOYMENT_TYPE_VM, getDigestAlgorithmForDeploymentTypeOrHashType(Constants.DEPLOYMENT_TYPE_VM, Constants.HASH_TYPE_VM).value());
 		deploymentTypeHashTypeMap.put(Constants.DEPLOYMENT_TYPE_BAREMETAL,
-				properties.getProperty(Constants.HASH_TYPE_BAREMETAL));
+				getDigestAlgorithmForDeploymentTypeOrHashType(Constants.DEPLOYMENT_TYPE_BAREMETAL, Constants.HASH_TYPE_BAREMETAL).value());
 		deploymentTypeHashTypeMap.put(Constants.DEPLOYMENT_TYPE_DOCKER,
-				properties.getProperty(Constants.HASH_TYPE_DOCKER));
+				getDigestAlgorithmForDeploymentTypeOrHashType(Constants.DEPLOYMENT_TYPE_DOCKER, Constants.HASH_TYPE_DOCKER).value());
 	}
 
 	public static Map<String, String> getAllHashTypes() {
@@ -29,18 +33,44 @@ public class ImageDeploymentHashTypeCache extends DirectorPropertiesCache {
 	}
 
 	public static DigestAlgorithm getDigestAlgorithmForHashType(String hashType) {
+		if(hashType == null){
+			return null;
+		}
 		DigestAlgorithm algorithm = null;
-		switch (hashType) {
-		case "SHA-1":
+		try {
+			algorithm = DigestAlgorithm.fromValue(hashType.toLowerCase());
+		} catch (IllegalArgumentException argumentException) {
+			log.error("No matching digest alg found for hashtype {}", hashType);
+		}
+		return algorithm;
+	}
+
+	public static DigestAlgorithm getDigestAlgorithmForDeploymentType(String deploymentType) {
+		if(deploymentType == null){
+			return null;
+		}
+		DigestAlgorithm algorithm = null;
+		switch (deploymentType) {
+		case Constants.DEPLOYMENT_TYPE_BAREMETAL:
 			algorithm = DigestAlgorithm.SHA_1;
 			break;
-		case "SHA-256":
+		case Constants.DEPLOYMENT_TYPE_DOCKER:
 			algorithm = DigestAlgorithm.SHA_256;
 			break;
-		case "MD5":
-			algorithm = DigestAlgorithm.MD_5;
+		case Constants.DEPLOYMENT_TYPE_VM:
+			algorithm = DigestAlgorithm.SHA_256;
 			break;
 		}
 		return algorithm;
 	}
+	
+	public static DigestAlgorithm getDigestAlgorithmForDeploymentTypeOrHashType(String deploymentType, String hashType) {
+		DigestAlgorithm digestAlgorithm = getDigestAlgorithmForHashType(hashType) ;
+		
+		if(digestAlgorithm == null){
+			digestAlgorithm = getDigestAlgorithmForDeploymentType(deploymentType);
+		}
+		return digestAlgorithm;
+	}
+
 }
