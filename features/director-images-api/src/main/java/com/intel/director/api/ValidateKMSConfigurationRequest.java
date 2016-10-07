78698cbf4c03954dce43f981fe926cfe79cace4f
Animesh Patel <animeshx.patel@intel.com>
2016-10-07 15:51:02 +0530

    Added new file for KMS, MTW validation

---
+++ features/director-images-api/src/main/java/com/intel/director/api/ValidateKMSConfigurationRequest.java
@@ -0,0 +1,32 @@
+package com.intel.director.api;
+
+public class ValidateKMSConfigurationRequest {
+	private String url;
+	private String user;
+	private String sha1;
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getSha1() {
+		return sha1;
+	}
+
+	public void setSha1(String sha1) {
+		this.sha1 = sha1;
+	}
+
+}
