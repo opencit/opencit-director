503222bdafe14406678dbadb468ad2fb9f4df38c
Oak, SiddharthX <siddharthx.oak@intel.com>
2017-01-30 15:00:49 +0530

    v3.1-rc1+next: Fix for 6272. Remove unused fields from UI
    Change-Id: I0ba84a676c914ba7f875514028b06e23d7dffc63

--- features/director-images-api/src/main/java/com/intel/director/api/MountWilsonSetting.java
+++ features/director-images-api/src/main/java/com/intel/director/api/MountWilsonSetting.java
@@ -11,19 +11,16 @@ public class MountWilsonSetting {
 	public String mtwilson_api_username;
 	public String mtwilson_api_password;
 	public String mtwilson_api_tls_policy_certificate_sha256;
-	public String mtwilson_server;
-	public String mtwilson_server_port;
-	public String mtwilson_username;
-	public String mtwilson_password;
+//	public String mtwilson_server;
+//	public String mtwilson_server_port;
+//	public String mtwilson_username;
+//	public String mtwilson_password;
 
 	@Override
 	public String toString() {
 		return "{\"mtwilson_api_url\":\"" + mtwilson_api_url + "\",\"mtwilson_api_username\":\"" + mtwilson_api_username
 				+ "\",\"mtwilson_api_password\":\"" + mtwilson_api_password
-				+ "\",\"mtwilson_api_tls_policy_certificate_sha256\":\"" + mtwilson_api_tls_policy_certificate_sha256
-				+ "\",\"mtwilson_server\":\"" + mtwilson_server + "\",\"mtwilson_server_port\":\""
-				+ mtwilson_server_port + "\",\"mtwilson_username\":\"" + mtwilson_username
-				+ "\",\"mtwilson_password\":\"" + mtwilson_password + "\"}";
+				+ "\",\"mtwilson_api_tls_policy_certificate_sha256\":\"" + mtwilson_api_tls_policy_certificate_sha256 + "\"}";
 	}
 
 	public String getMtwilson_api_url() {
@@ -58,37 +55,6 @@ public class MountWilsonSetting {
 		this.mtwilson_api_tls_policy_certificate_sha256 = mtwilson_api_tls_policy_certificate_sha56;
 	}
 
-	public String getMtwilson_server() {
-		return mtwilson_server;
-	}
-
-	public void setMtwilson_server(String mtwilson_server) {
-		this.mtwilson_server = mtwilson_server;
-	}
-
-	public String getMtwilson_server_port() {
-		return mtwilson_server_port;
-	}
-
-	public void setMtwilson_server_port(String mtwilson_server_port) {
-		this.mtwilson_server_port = mtwilson_server_port;
-	}
-
-	public String getMtwilson_username() {
-		return mtwilson_username;
-	}
-
-	public void setMtwilson_username(String mtwilson_username) {
-		this.mtwilson_username = mtwilson_username;
-	}
-
-	public String getMtwilson_password() {
-		return mtwilson_password;
-	}
-
-	public void setMtwilson_password(String mtwilson_password) {
-		this.mtwilson_password = mtwilson_password;
-	}
 
 	public String validate() {
 		List<String> errors = new ArrayList<>();
