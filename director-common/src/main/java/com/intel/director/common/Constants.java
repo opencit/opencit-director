bd1cfd1f66ce8412be2cb1f38e7faabfcdcc5726
Ryan Savino <ryan.k.savino@intel.com>
2017-03-10 10:33:59 -0800

    v3.1-rc1+next: missing HOST_TYPE variables in Constants.java

--- director-common/src/main/java/com/intel/director/common/Constants.java
+++ director-common/src/main/java/com/intel/director/common/Constants.java
@@ -255,7 +255,10 @@ public class Constants {
 	public static final String TREE_ELEMENT_DIRECTORY="directory";
 	public static final String TREE_ELEMENT_FILE="file";
 	public static final String TREE_ELEMENT_SYMLINK="symlink";
-
+        
+        public static final String HOST_TYPE_WINDOWS = "Windows";
+        public static final String HOST_TYPE_LINUX = "Linux";
+        
 	///public final static String IMAGE_FORMAT_WINDOWS = "Microsoft Disk Image";
 	//public final static String IMAGE_FORMAT_LINUX = "QCOW";
 	public static final String IMAGE_FORMAT_RESULT_RAW="x86 boot sector";
