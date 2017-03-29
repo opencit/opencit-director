66130c136237b2d6d887c65459f8c2a9f6876401
Aakash <aakashmaheshwari2@gmail.com>
2017-03-29 16:24:20 +0530

    Fix for showing correct date time in grid

--- features/director-images-api/src/main/java/com/intel/director/api/AuditFields.java
+++ features/director-images-api/src/main/java/com/intel/director/api/AuditFields.java
@@ -2,18 +2,19 @@ package com.intel.director.api;
 
 import java.util.Calendar;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 public class AuditFields {
 
 	protected String created_by_user_id;
 	
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+	@JsonSerialize(using = DirectorCalenderSerializer.class)
 	protected Calendar created_date;
 
 	protected String edited_by_user_id;
 	
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+	@JsonSerialize(using = DirectorCalenderSerializer.class)
 	protected Calendar edited_date;
 
 	public AuditFields(String created_by_user_id, Calendar created_date,
